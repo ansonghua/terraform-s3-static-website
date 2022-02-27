5519ff75a8ca98b187f644da51fe855caa3eac47
Amy An <ansonghua@hotmail.com>
2022-02-27 07:07:19 -0600

    initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-amy"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
